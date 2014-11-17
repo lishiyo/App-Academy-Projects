8e3f4acc9e2e7404ca008d3f1b7aa9b464551795
Connie Li <c.ziye.li@gmail.com>
2014-11-17 13:56:13 -0500

    Allows game to be won or lost

--- game.rb
+++ game.rb
@@ -2,18 +2,24 @@ require './minesweeper.rb'
 
 class Game
 
+  attr_accessor :board
+
   def initialize
     run_game
   end
 
   def run_game
     @board = Board.new
-    loop do
+    until over?
       render
       position = gets.chomp.split(" ").map(&:to_i)
       @board[position[0], position[1]].reveal
     end
+
+    finish_game
   end
+
+  private
 
   def render
     @board.grid.each_with_index do |row, row_i|
@@ -33,4 +39,20 @@ class Game
     end
   end
 
+  def over?
+    won? || lost?
+  end
+
+  def won?
+    @board.grid.flatten.none?{|tile| tile.revealed? == false && tile.bombed? == false}
+  end
+
+  def lost?
+    @board.grid.flatten.any?{|tile| tile.revealed? && tile.bombed? }
+  end
+
+  def finish_game
+    puts won? ? "You won. Play again? (y/n)" : "You lost. Play again? (y/n)"
+    run_game if gets.chomp == "y"
+  end
 end
