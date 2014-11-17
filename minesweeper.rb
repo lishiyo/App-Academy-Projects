3e3d190a3b76ed48e1c4dfc706c94d212f0f8188
Connie Li <c.ziye.li@gmail.com>
2014-11-17 11:26:15 -0500

    creates Tile class with neighbors method

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,13 +1,50 @@
 class Board
-  def initialize (board = Board.create_board)
-    @board = board
+  attr_reader :size
+
+  def initialize (size = 9)
+    @board = Board.create_board(size)
+    @size = size
   end
 
-  def self.create_board
-    Array.new(9){ Array.new (9) }
+  def self.create_board(size)
+    Array.new(size){ Array.new (size) }
   end
 
   def seed_board
 
   end
+
+  def [] (position)
+    @board[position[0]][position[1]]
+  end
+end
+
+class Tile
+
+  INCREMENTS = [0,1,-1,1,-1].permutation(2).to_a.uniq
+
+  attr_reader :position
+
+  def initialize (board, position)
+    @board = board
+    @position = position
+    @bombed = false
+    @flagged = false
+    @revealed = false
+  end
+
+  def neighbors
+    neighbors = []
+    INCREMENTS.each do |increment|
+      new_pos = [position[0] + increment[0], position[1] + increment[1]]
+      if new_pos[0].between?(0, @board.size - 1) &&
+        new_pos[1].between?(0, @board.size - 1)
+        neighbors << new_pos
+      end
+    end
+
+    neighbors
+  end
+
+
 end
