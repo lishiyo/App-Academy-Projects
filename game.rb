a792f176df2385d278691402062ed25fdced2eba
Connie Li <c.ziye.li@gmail.com>
2014-11-17 12:35:16 -0500

    Adds game.rb, renders game.

---
+++ game.rb
@@ -0,0 +1,32 @@
+require './minesweeper.rb'
+
+class Game
+
+  def initialize
+    run_game
+  end
+
+  def run_game
+    @board = Board.new
+    render
+  end
+
+  def render
+    @board.grid.each_with_index do |row, row_i|
+      row.each_with_index do |tile, col_i|
+        if tile.bombed? && tile.revealed?
+          print "☠ "
+        elsif tile.flagged?
+          print "⚑ "
+        elsif tile.revealed?
+          bomb_count = tile.neighbor_bomb_count
+          print "#{bomb_count.zero? ? □ : bomb_count}"
+        else
+          print "■ "
+        end
+      end
+      print "\n"
+    end
+  end
+
+end
