bf44a2252f2dd6f8299e2fe334cfc3006b247a9a
chatelet <Jesus / Mike @appacademy>
2014-11-20 10:15:47 -0500

    refactors Board to be DRYer

--- board.rb
+++ board.rb
@@ -1,62 +1,86 @@
-require './piece.rb'
+require_relative 'piece'
+
 
 class Board
-	
-	attr_reader :grid
-	
-	def initialize(fill_board = true)
-		create_grid(fill_board)
+
+	COLOR_ROWS = {
+		:top => :red,
+		:bottom => :black
+	}
+
+	attr_reader :grid, :size
+
+	def initialize(fill_board = true, size = 8)
+		@size = size
+		create_grid(fill_board, size)
 	end
-	
+
 	def [](pos)
+		raise CheckersError.new("Not in board bounds!") unless Board.in_bounds?(pos)
+
 		x, y = pos[0], pos[1]
 		@grid[x][y]
 	end
-	
+
 	def []=(pos, mark)
+		raise CheckersError.new("Not in board bounds!") unless Board.in_bounds?(pos)
+
 		x, y = pos[0], pos[1]
 		@grid[x][y] = mark
 	end
-	
+
+	def add_piece(piece, pos)
+		raise CheckersError.new("Position not empty") unless empty?(pos)
+
+		self[pos] = piece
+	end
+
+	def empty?(pos)
+		self[pos].nil?
+	end
+
 	def self.in_bounds?(pos)
 		pos.all?{|coord| coord.between?(0, 7) }
 	end
-	
+
 	def dup
-		duped_board = Board.new(false) # blank grid
+		duped_board = Board.new(false, self.size) # blank grid
 		pieces.each do |piece| # instantiate new pieces with same pos and color
-			piece.class.new(duped_board, piece.pos, piece.color)
+			piece.class.new(duped_board, piece.pos.dup, piece.color)
 		end
-		
+
 		duped_board
 	end
-	
+
 	def inspect
-		grid = @grid.map{|row| row.map{|square| square.nil? ? square : square.color } }
-		
-		grid.join("\n")
+		@grid.each do |row|
+			puts "#{ row.map{ |square| square.nil? ? square : square.color } }"
+		end
 	end
-	
+
 	protected
-	
+
 	def pieces
 		@grid.flatten.compact
 	end
-	
-	def create_grid(fill_board)
-		@grid = Array.new(8) { Array.new(8) } # stop here unless place pieces
-		return unless fill_board
+
+	def create_grid(fill_board, size)
+		@grid = Array.new(size) { Array.new(size) }
+		return unless fill_board  # stop here unless place pieces
 		# fill with red and black pieces
+
+
 		@grid.each_with_index do |row, row_i|
 			row.each_with_index do |col, col_j|
-				if row_i.between?(0, 2) # red pieces
-						@grid[row_i][col_j] = Piece.new(self, [row_i, col_j], :red) if (row_i + col_j).odd?
-				elsif row_i.between?(5, 7) # black pieces
-					@grid[row_i][col_j] = Piece.new(self, [row_i, col_j], :black) if (row_i + col_j).odd?
+				fill_row = Proc.new do |color|
+					@grid[row_i][col_j] = Piece.new(self, [row_i, col_j], color) if (row_i + col_j).odd?
 				end
+
+				fill_row.call(COLOR_ROWS[:top]) if row_i.between?(0, 2) # red pieces
+				fill_row.call(COLOR_ROWS[:bottom]) if row_i.between?(5, 7) # black pieces
 			end
 		end
-		
+
 	end
-	
+
 end
--- piece.rb
+++ piece.rb
@@ -1,40 +1,41 @@
-require './board.rb'
+require_relative 'board'
 
-class InvalidMoveError < StandardError
+class CheckersError < StandardError
+end
+
+class InvalidMoveError < CheckersError
 end
 
 class Piece
-	
+
 	attr_accessor :pos, :color, :now_king
 	attr_reader :board
-	
+
 	def initialize(board, pos, color)
 		@now_king = false
-		@board = board
-		@pos = pos
-		@color = color
-		
+		@board, @pos, @color = board, pos, color
+
 		# put self on board
-		@board[self.pos] = self
+		board.add_piece(self, pos)
 	end
-	
+
 	# one move diagonally
 	def perform_slide(end_pos)
 		deltas = move_diffs
 		# both empty and in bounds
 		valid_positions = deltas.map{ |(dx, dy)| [pos[0] + dx, pos[1] + dy] }
 		.select{|pos| self.board[pos].nil? && Board.in_bounds?(pos) }
-		
+
 		puts "perform_slide valid pos: #{valid_positions}"
 		raise InvalidMoveError.new("You can't slide there!") unless valid_positions.include?(end_pos)
-		
+
 		self.board[end_pos] = self # add self to new position on board
 		self.board[pos] = nil # delete old position on board
 		self.pos = end_pos # update self's pos
-		
+
 		@now_king = true if should_promote?
 	end
-	
+
 	# should remove the jumped over piece from the Board
 	def perform_jump(end_pos)
 		deltas = move_diffs.map{ |x,y| [x * 2, y * 2] }
@@ -42,14 +43,14 @@ class Piece
 		# both empty and in bounds
 		valid_positions = deltas.map{ |(dx, dy)| [posx + dx, posy + dy] }
 		.select{|pos| board[pos].nil? && Board.in_bounds?(pos) }
-		
+
 		puts "perform_jump valid pos: #{valid_positions}, #{valid_positions.include?(end_pos)}, end_pos: #{end_pos}"
-		
+
 		raise InvalidMoveError.new("You can't jump there!") unless valid_positions.include?(end_pos)
-		
+
 		piece_between = board[[((posx + end_pos[0]) / 2), ((posy + end_pos[1]) / 2)]]
 		raise InvalidMoveError.new("Can't jump over that place!") unless piece_between && piece_between.color != self.color
-		
+
 		#puts "perform_jump piece between: #{piece_between.pos}"
 
 		# else, you can jump there
@@ -57,30 +58,30 @@ class Piece
 		self.board[pos] = nil # delete old position on board
 		self.pos = end_pos # update self's pos
 		board[piece_between.pos] = nil # delete the enemy piece
-		
+
 		@now_king = true if should_promote?
 	end
-	
+
 	# checks to see if piece has reached back row
 	def should_promote?
 		(self.color == :red) ? (board[pos] == 7) : (board[pos] == 1)
 	end
-	
+
 	# First checks valid_move_seq?
 	# either calls perform_moves! OR raises an InvalidMoveError
 	def perform_moves(move_sequence)
 		raise InvalidMoveError.new("That move sequence was invalid!") unless valid_move_seq?(move_sequence)
-		
+
 		perform_moves!
 	end
-	
+
 	# 1) one slide, or 2) one or more jumps
 	# If the sequence is one move long, try sliding; if that doesn't work, try jumping
 	# If the sequence is multiple moves long, every move must be a jump.
 	# Raise InvalidMoveError if a move in the sequence fails
 	def perform_moves!(move_sequence)
 		# move_sequence = [[2,1]] <- array of positions
-	
+
 		if move_sequence.size == 1 # one move, slide or jump
 			end_pos = move_sequence.first
 			p end_pos
@@ -93,34 +94,39 @@ class Piece
 		else # must all be jumps
 			move_sequence.each { |end_pos| perform_jump(end_pos) }
 		end
-		
+
 	end
-	
+
 	# calls perform_moves! on a *duped* Piece/Board
 	# returns true only if no error is raised
 	def valid_move_seq?(move_sequence)
 		begin
 			duped_board = board.dup
-			duped_piece = duped_board[self.pos] # doppleganger on duped_board
+			duped_piece = duped_board[self.pos.dup] # doppleganger on duped_board
 			duped_piece.perform_moves!(move_sequence)
 		rescue InvalidMoveError
 			return false
 		end
-		
+
 		true
 	end
-	
+
 	def inspect
 		"#{@color}#{@now_king}#{pos}"
 	end
-	
+
+	# unicode checkers
+	def render
+
+	end
+
 	private
-	
+
 	def move_diffs
 		deltas = { red: [[1, 1], [1, -1]], black: [[-1, 1], [-1, -1]] }
-		
+
 		return deltas[self.color] unless @now_king
-		
+
 		# if king, return both
 		deltas[:red] + deltas[:black]
 	end
