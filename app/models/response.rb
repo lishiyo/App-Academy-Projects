1f2a5e47f7f958ed9461cdf976f03f0e51e3937b
Connie Li <c.ziye.li@gmail.com>
2014-12-01 11:43:30 -0500

    add poll results

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,6 +3,7 @@ class Response < ActiveRecord::Base
   validates :respondent_id, presence: true
   validates :answer_choice_id, presence: true
   validate :respondent_has_not_already_answered_question
+  validate :author_cannot_respond_to_own_poll
 
   belongs_to :answer_choice, class_name: "AnswerChoice", foreign_key: :answer_choice_id
   belongs_to :respondent, class_name: "User", foreign_key: :respondent_id
@@ -12,14 +13,22 @@ class Response < ActiveRecord::Base
 
   #  other Response objects for the same Question
   def sibling_responses
-    self.question.responses
-      .where('? IS NOT NULL AND responses.id != ?', self.id)
+    self.question.responses.where.not(id: nil).where.not(id: id)
   end
 
 
   private
 
   def respondent_has_not_already_answered_question
-
+    if sibling_responses.exists?(respondent_id: self.respondent_id)
+      errors[:respondent] << "Can't have already answered question"
+    end
   end
+
+  def author_cannot_respond_to_own_poll
+    if self.answer_choice.question.poll.author_id == respondent_id
+      errors[:respondent] << "Author cannot respond to own poll"
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,30 @@ class User < ActiveRecord::Base
   has_many :authored_polls, class_name: "Poll", foreign_key: :author_id
   has_many :responses, class_name: "Response", foreign_key: :respondent_id
 
+  def completed_polls
+      # SQL
+      # SELECT polls.*, COUNT(questions.id) AS questions_count, COUNT(res.id) as res_count
+      # FROM polls INNER JOIN questions ON questions.poll_id = polls.id
+      # INNER JOIN answer_choices ON answer_choices.question_id = questions.id
+      # LEFT JOIN
+      #   (SELECT *
+      #     FROM responses
+      #     WHERE responses.respondent_id = ?
+      # ) AS res ON res.answer_choice_id = answer_choices.id
+      # GROUP BY polls.id
+      # HAVING COUNT(questions.id) = COUNT(res.id)
+
+      Poll.find_by_sql("
+        SELECT polls.*, COUNT(DISTINCT questions.id) AS questions_count, COUNT(res.id) as res_count
+        FROM polls INNER JOIN questions ON questions.poll_id = polls.id
+        INNER JOIN answer_choices ON answer_choices.question_id = questions.id
+        LEFT JOIN
+          (SELECT *
+            FROM responses
+            WHERE responses.respondent_id = #{self.id}
+        ) AS res ON res.answer_choice_id = answer_choices.id
+        GROUP BY polls.id
+        HAVING COUNT(DISTINCT questions.id) = COUNT(res.id)")
+  end
+
 end
