564f2a51b9ef24ffe4c76f5d6d226c6e9e9221ed
Anthony Chang <roykillany@gmail.com>
2014-12-03 20:27:13 -0500

    currently in AuthDemo Part II

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthDemo::Application.config.secret_key_base = '3c99193d52d70c246c48c6ae429038dca672ae809a4b076d3b247083ab7c6550604a9f79287b41e896504fd83ed8a6a024300d2bb987554f88450a2751c91515'
