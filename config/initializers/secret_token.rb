1310c4330814c98cf0ddffcee3099b949e7becdb
Connie Li <c.ziye.li@gmail.com>
2014-12-10 10:10:35 -0500

    set up gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goalie::Application.config.secret_key_base = 'df829e50a3fe6aa5cce3dccadff075331d28cd365b3efcb4f6a2d22ac9e4cfbc301a35ea2c38e91ff2bc91f921b6cecd1622fc40d23f2aba15a4a9deb5c5a458'
