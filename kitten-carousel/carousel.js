aff35a6c3d3e3735011b2f2bcdb58bfba096bdbd
App Academy Student <appacademy@Apps-Mac-mini-10.local>
2014-12-18 15:48:59 -0500

    finishes thumbnails

--- kitten-carousel/carousel.js
+++ kitten-carousel/carousel.js
@@ -7,6 +7,7 @@
     this.$el = $(el);
     this.activeIndex = 0;
     this.$items = this.$el.find("img");
+    this.transitioning = false;
 
     $('a.slide-left').on('click', this.slideLeft.bind(this)); // Next
     $('a.slide-right').on('click', this.slideRight.bind(this)); // Prev
@@ -14,23 +15,39 @@
   };
 
   $.Carousel.prototype.slide = function(dir) {
+    if (this.transitioning) { return; }
+
+    this.transitioning = true;
     var $oldCorgi = this.$items.eq(this.activeIndex);
 
-
-
-    $oldCorgi.removeClass('active left right');
-
+    // grab nextCorgi
     var nextIndex = (this.activeIndex + dir + this.$items.length) % this.$items.length;
     var $newCorgi = this.$items.eq(nextIndex);
 
-    var direction = (dir===1) ? "left" : "right"
+    if (dir===1) {
+      var newDir = "left";
+      var oldDir = "right";
+    }  else {
+      var newDir = "right";
+      var oldDir = "left";
+    }
 
-    $newCorgi.addClass(direction).addClass("active");
+    $oldCorgi.addClass(oldDir);
+    $newCorgi.addClass(newDir).addClass("active");
+
+    // trigger newCorgi's change to left: 0%
+    setTimeout(function () {
+      $newCorgi.removeClass(newDir);
+    }, 0);
+
+    // after oldCorgi finishes moving left/right, go back to 0% and set display:none
+    $oldCorgi.one('transitionend', function(event){
+      $oldCorgi.removeClass('active').removeClass(oldDir);
+      this.transitioning = false;
+    }.bind(this));
 
     this.activeIndex = nextIndex;
-    setTimeout(function () {
-      $newCorgi.removeClass(direction);
-    }, 0);
+
   }
 
   // Next => 1
