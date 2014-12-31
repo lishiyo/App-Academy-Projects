7d985a8ea6b2ec00b10bba0d52087a9c19e93d6d
lishiyo <c.ziye.li@gmail.com>
2014-12-31 17:56:38 -0500

    fixes sortable

--- skeleton/app/assets/javascripts/views/board_show.js
+++ skeleton/app/assets/javascripts/views/board_show.js
@@ -3,7 +3,7 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   template: JST['boards/show'],
 
   initialize: function(){
-    this.listenTo(this.model, 'sync', this.render);
+    // this.listenTo(this.model, 'sync', this.render);
 
     this.listenTo(this.model.lists(), 'sync', this.render);
 
